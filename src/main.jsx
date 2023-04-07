f2d563f2d43f1cb045481679bb96cd2a472ebd2b
lizaafrin <lizaafrin446@gmail.com>
2023-04-07 15:08:24 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import MainLayout from './Components/Layout/MainLayout';
+import OrderReview from './Components/OrderReview/OrderReview';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch('tShirts.json')
+      },
+      {
+        path: "/review",
+        element: <OrderReview></OrderReview>,
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
